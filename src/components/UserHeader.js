d844e0d9f592ec533cd59aa7f4d7355c9ec9334a
Brian Purnell <purnellbp@gmail.com>
2020-07-11 21:17:17 -0500

    updated fetch actions to use lodash memoize.

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,26 +1,26 @@
 import React from "react";
 import {connect} from "react-redux";
-import { fetchUser} from "../actions";
-
-class UserHeader extends React.Component{
-   componentDidMount() {
-       this.props.fetchUser(this.props.userID);
-   }
-
-   render() {
-       const user = this.props.users.find(user=> user.id === this.props.userID);
-
-       if(!user){
-           return null;
-       }
+import {fetchUser} from "../actions";
 
 
-       return <div className="header" > {user.name}</div>
-   }
+class UserHeader extends React.Component {
+    componentDidMount() {
+        this.props.fetchUser(this.props.userID);
+    }
+
+    render() {
+        const {user} = this.props;
+
+        if (!user) {
+            return null;
+        }
+
+        return <div className="header"> {user.name}</div>
+    }
 }
 
-const mapStateToProps = (state) => {
-    return {users: state.users};
+const mapStateToProps = (state, ownProps) => {
+    return {user: state.users.find(user => user.id === ownProps.userId)};
 };
 
 export default connect(
